6865c259ce13cd144734708dcc988afb36e28bb6
Xurana <xurana@gmail.com>
2022-06-24 15:15:10 +0700

    HT_1

---
+++ HT_1/HomeTask_1.1.py
@@ -0,0 +1,10 @@
+j = 12
+k = 44
+print(j, k)
+j, k = k, j
+print(j, k)
+
+name = input("Введите Ваше имя: ")
+age = (input("Сколько Вам лет? "))
+password = (input("Напишите пароль: "))
+print(f"Здравствуйте, {name}! Вам {age} лет. Пароль {password}")
